43eb90cb944ea69839cf641381ac37ad0663abb7
sponrad <sponrad@gmail.com>
2022-05-31 20:57:51 -0700

    localstorage: models: Put some data in async storage
    To save space since we are limited to 2048 bytes in secure storage.
    
    Basically everything except quantities can go in async storage.

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,4 +1,5 @@
 import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Asset, InterestAccount } from './models.js';
 
 async function save(key, value) {
@@ -13,6 +14,20 @@ async function getValueFor(key) {
     return result;
 }
 
+
+async function saveInsecure(key, value) {
+    await AsyncStorage.setItem(key, value);
+}
+
+
+async function getInsecureValueFor(key) {
+    let result = await AsyncStorage.getItem(key);
+    if (!result) {
+        return '[]';
+    }
+    return result;
+}
+
 const assetsKey = 'local-storage-asset-key';
 
 export async function getAssets() {
@@ -20,46 +35,89 @@ export async function getAssets() {
     if (!assetsData) {
         return [];
     }
-    return JSON.parse(assetsData)
-               .map(json => {
-                   return new Asset(
-                       json.name,
-                       json.symbol,
-                       json.imageUrl,
-                       json.quantity,
-                       json.interestAccounts.map(
-                           ia => {
-                               return new InterestAccount(
-                                   ia.name,
-                                   ia.interestTiers,
-                                   ia.quantity,
-                               );
-                           }
-                       ),
-                   );
-               });
+    let combinedData = [];
+    combinedData = JSON.parse(assetsData);
+    const nonsecureData = await getInsecureValueFor(assetsKey);
+    // pile in the nonsecure data
+    JSON.parse(nonsecureData).forEach((json, idx) => {
+        // TODO rename to abbreviated forms
+        if (combinedData[idx].symbol !== json.symbol) {
+            return;
+        }
+        combinedData[idx] = {
+            ...combinedData[idx],
+            ...json,
+        };
+    })
+    return combinedData.map(json => {
+        return new Asset(
+            json.name,
+            json.symbol,
+            json.imageUrl,
+            // TODO rename these to abbreviated forms
+            json.quantity,
+            json.interestAccounts.map(
+                ia => {
+                    return new InterestAccount(
+                        ia.name,
+                        ia.interestTiers,
+                        ia.quantity,
+                    );
+                }
+            ),
+        );
+    });
 }
 
 export function saveAssets(assets) {
-    save(assetsKey,
-         JSON.stringify(assets.map(asset => {
-             return {
-                 name: asset.name,
-                 symbol: asset.symbol,
-                 imageUrl: asset.imageUrl,
-                 quantity: asset.quantity,
-                 interestAccounts: asset.interestAccounts.map(
-                     ia => {
-                         return {
-                             name: ia.name,
-                             interestTiers: ia.interestTiers,
-                             quantity: ia.quantity,
-                         };
-                     }
-                 ),
-             };
-         }))
-    );
+    const jsonNonsecureToSave = JSON.stringify(assets.map(asset => {
+        return {
+            name: asset.name,
+            symbol: asset.symbol,
+            imageUrl: asset.imageUrl,
+            s: asset.symbol,
+            n: asset.name,
+            i: asset.imageUrl,
+        };
+    }));
+    saveInsecure(assetsKey, jsonNonsecureToSave);
+    const jsonToSave = JSON.stringify(assets.map(asset => {
+        // TODO get rid of these doubled up long-name versions
+        // abbreviating these keys to save SecureStore space
+        return {
+            symbol: asset.symbol,
+            quantity: asset.quantity,
+            interestAccounts: asset.interestAccounts.map(
+                ia => {
+                    return {
+                        name: ia.name,
+                        interestTiers: ia.interestTiers,
+                        quantity: ia.quantity,
+                    };
+                }
+            ),
+            s: asset.symbol,
+            q: asset.quantity,
+            // interestAccounts
+            i: asset.interestAccounts.map(
+                ia => {
+                    return {
+                        // name
+                        n: ia.name,
+                        // interestTiers
+                        t: ia.interestTiers,
+                        // quantity
+                        q: ia.quantity,
+                    };
+                }
+            ),
+        };
+    }));
+    // console.log(jsonToSave.length);
+    // console.log(jsonToSave);
+    // Provided value to SecureStore is larger than 2048 bytes. An attempt to
+    // store such a value will throw an error in SDK 35.
+    save(assetsKey, jsonToSave);
 }
 
 const authTimeKey = 'auth-time-key';
